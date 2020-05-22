bf5e4858e74c360a4f808735ebba221ba6ce1844
Sunny Patel <patel.sunny293@gmail.com>
2020-05-22 13:49:55 -0400

    prepared for deployment

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const app = express();
+const path = require('path')
 const connectDB = require('./config/db');
 const users = require('./routes/api/users');
 const auth = require('./routes/api/auth');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 const notifications = require('./routes/api/notifications');
+
+
+const app = express();
 
 // connect DB
 connectDB();
@@ -19,6 +22,16 @@ app.use('/api/auth', auth);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 app.use('/api/notifications', notifications);
+
+// Serve Static Assets in Production
+if(process.env.NODE_ENV === 'production') {
+  // Set Static Folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 
